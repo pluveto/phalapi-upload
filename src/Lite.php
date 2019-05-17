f2ff7956c36680b65a3074a18fbcbd43e975ed1a
pluveto <reg@pluvet.com>
2019-05-17 14:37:32 +0800

    update

--- src/Lite.php
+++ src/Lite.php
@@ -2,6 +2,63 @@
 namespace PhalApi\Upload;
 
 /**
- * 文件上传类库, 移植自 ThinkPHP, 所有权限归
+ * 使用方法:
+ *      ```
+ *      // 载入文件上传插件
+ *      $di->upload = new \PhalApi\Upload\Lite();
+ *      // 重写请求处理器     
+ *      $di->upload->saveImage(file,config)
+ *      $di->upload->saveFile(file,config)
+ *      
+ *      ```
+ * 
  * @author ZhangZijing <i@pluvet.com>
  */
+class Upload
+{
+    /**
+     * 设置:
+     *      上传目录
+     *      
+     *
+     */
+
+    /**
+     * 检测文件的 MIME 类型
+     * https://www.php.net/manual/zh/function.mime-content-type.php
+     */
+
+    private $_uploadPath;
+    private $_allowTypes;
+
+    private $_imgMinWidth;
+    private $_imgMinHeight;
+    private $_imgMaxWidth;
+    private $_imgMaxHeight;
+
+    private $_imgWaterMarkPath;
+    private $_imgWaterMarkPosition; // 与数字小键盘相同
+
+    private $_overwrite = false;
+
+    private $_detectMime = false;
+
+    /**
+     * 处理单个图片上传
+     *
+     * @param array $file 文件信息数组
+     * @return void
+     */
+    public function uploadImage($file)
+    {
+        // 
+    }
+    /**
+     * 获得随机文件名
+     *
+     * @param 扩展名 $ext
+     * @return string
+     */
+    public function getRandomFilename($ext = '')
+    { }
+}
---
+++ src/Request.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace PhalApi\Upload;
+
+/**
+ * 该类用于自定义返回格式接口。
+ *  - 重写后，请求地址变成形如 `/index.php?r=Namespace/Class/Action` 
+ *  - 参考：http://docs.phalapi.net/#/v2.0/how-to-request
+ * 
+ * 提示：通过 Apache 或 Nginx 的进一步重写，可变成形如 `/Namespace/Class/Action`
+ * 
+ * 注意：实际请求时，Namespace 不写，因为在下面进行了处理，自动加上 App 命名空间
+ * 
+ * @author ZhangZijing <i@pluvet.com> 2019-5-14
+ */
+class Request extends \PhalApi\Request
+{
+
+    public function getService()
+    {
+        // 优先返回自定义格式的接口服务名称
+        $service = $this->get('r');
+        if (!empty($service)) {
+            // 去除首尾的 "/"
+            $service = trim($service, "\/");
+            return 'App.' . str_replace('/', '.', $service);
+        }
+
+        return parent::getService();
+    }
+}
